860101db1ca3d629d6e62ea2be08a7c88d9221bd
LeoDavila1994 <liodavila.ld@gmail.com>
2022-07-02 13:42:09 -0500

    bg random

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,18 @@ import './App.css'
 import Card from './Components/Card'
 
 // Pendientes:
-//1- Cambiar bg segun estado de clima.
 //2- Hacer pantalla de carga.
 //3- Usar un custom Hook.
 
 function App() {
 
     const [weatherApi, setWeatherApi] = useState({});
+    const [randomBg, setRandomBg] = useState("./src/assets/wp_1401.jpg");
+    const [dateBg, setDateBg] = useState(new Date());
+
+    function refreshBg() {
+        setDateBg(new Date());
+    }
 
     useEffect(() => {
 
@@ -28,13 +33,50 @@ function App() {
 
         navigator.geolocation.getCurrentPosition(succes);
 
-    }, []);
+                if (weatherApi.weather?.[0].id >= 200 && weatherApi.weather?.[0].id <= 232) {
+                    if (dateBg.getHours() <= 18) {
+                        setRandomBg("./src/assets/thunder_day.gif")
+                    }
+                    setRandomBg("./src/assets/thunder_nigth.gif")
+                } else if (weatherApi.weather?.[0].id >= 300 && weatherApi.weather?.[0].id <= 531) {
+                    if (dateBg.getHours() <= 18) {
+                        setRandomBg("./src/assets/raining_day.gif")
+                    }
+                    setRandomBg("./src/assets/raining_nigth.gif")
+                } else if (weatherApi.weather?.[0].id >= 600 && weatherApi.weather?.[0].id <= 622) {
+                    if (dateBg.getHours() <= 18) {
+                        setRandomBg("./src/assets/snow_day.gif")
+                    }
+                    setRandomBg("./src/assets/snow_nigth.gif")
+                } else if (weatherApi.weather?.[0].id >= 701 && weatherApi.weather?.[0].id <= 781) {
+                    if (dateBg.getHours() <= 18) {
+                        setRandomBg("./src/assets/mist_day.gif")
+                    }
+                    setRandomBg("./src/assets/mist_nigth.gif")
+                } else if (weatherApi.weather?.[0].id === 800) {
+                    if (dateBg.getHours() <= 18) {
+                        setRandomBg("./src/assets/clear_day.gif")
+                    }
+                    setRandomBg("./src/assets/clear_nigth.gif")
+                } else if (weatherApi.weather?.[0].id >= 801 && weatherApi.weather?.[0].id <= 804) {
+                    if (dateBg.getHours() <= 18) {
+                        setRandomBg("./src/assets/cloud_day.gif")
+                    }
+                    setRandomBg("./src/assets/cloud_nigth.gif")
+                }
+
+            const bgR = setInterval(refreshBg, 1000);
+            return function cleanUp() {
+            clearInterval(bgR);
+        }
+
+        }, []);
 
     let kelvin = (weatherApi.main?.temp);
 
     let celsius = (kelvin - 273.15).toFixed(1);
 
-    let fahrenheit = ((kelvin - 273.15) * 9/5 +32).toFixed(1);
+    let fahrenheit = ((kelvin - 273.15) * 9 / 5 + 32).toFixed(1);
 
     let icon = (weatherApi.weather?.[0].icon);
 
@@ -42,11 +84,9 @@ function App() {
 
     let description = (weatherApi.weather?.[0].description);
 
-    // style={{backgroundImage:`url(${"./src/assets/raining_nigth.gif"})`}}
-
     return (
-        <section>
-            <Card city={weatherApi.name} weather={weatherApi.weather?.[0].main} celsius={celsius} fahrenheit={fahrenheit} icon={icon} counrty={counrty} description={description}/>
+        <section style={{ backgroundImage: `url(${randomBg})` }}>
+            <Card city={weatherApi.name} weather={weatherApi.weather?.[0].main} celsius={celsius} fahrenheit={fahrenheit} icon={icon} counrty={counrty} description={description} />
         </section>
     )
 }
